d4c3cce792044c326e9f159289edeec9b6157e89
vieremds <vieremds@gmail.com>
2023-03-25 21:02:18 +0100

    heroku2

--- mooney/app/__init__.py
+++ mooney/app/__init__.py
@@ -4,6 +4,9 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
 from flask_bootstrap import Bootstrap
+import logging 
+from logging.handlers import RotatingFileHandler
+import os
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,7 +16,24 @@ login = LoginManager(app)
 login.login_view = 'login'
 bootstrap = Bootstrap(app)
 
-
-
 from app import routes, models
 
+def create_app(config_class=Config):
+    if not app.debug and not app.testing:
+
+        if app.config['LOG_TO_STDOUT']:
+            stream_handler = logging.StreamHandler()
+            stream_handler.setLevel(logging.INFO)
+            app.logger.addHandler(stream_handler)
+        else:
+            if not os.path.exists("logs"):
+                os.makedir("logs")
+            file_handler = RotatingFileHandler("logs/mooney.log", maxBites=10240, backupCount =10)
+            file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+
+        app.logger.setLevel(logging.INFO)
+        app.logger.info('Mooney startup')
+    
+    return app
