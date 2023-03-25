11b1bed259402cdb74aa69147a197f18cdee9ced
vieremds <vieremds@gmail.com>
2023-03-25 20:53:44 +0100

    heroku

--- mooney/config.py
+++ mooney/config.py
@@ -3,9 +3,10 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace('postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     DEBUG = True
     ACCOUNT_TYPES = ['Giro','Liability','Investment']
     CURRENCIES = [('EUR', 'Euro'), ('BRL', 'Brasilian-Real'), ('USD', 'Dollar'), ('GBP','British-Pounds')]
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
